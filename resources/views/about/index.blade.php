dedb94c72cc2cece5106fa45ce0395516cb9bb98
jevalencia887 <62039322+jevalencia887@users.noreply.github.com>
2020-03-23 00:09:29 -0500

    EVO

--- resources/views/about/index.blade.php
+++ resources/views/about/index.blade.php
@@ -1,51 +1,8 @@
 @extends('layouts.layout')
 
-@section('Name_header')
-   
-@endsection
 
-@section('content')
-
-
-    <!-- Mobile Menu  -->
-    <div class="mobile-menu">
-        <div class="menu-backdrop"></div>
-        <div class="close-btn"><i class="fas fa-times"></i></div>
-        
-        <nav class="menu-box">
-            <div class="nav-logo"><a href="index.html"><img src="images/logo.png" alt="" title=""></a></div>
-            <div class="menu-outer"><!--Here Menu Will Come Automatically Via Javascript / Same Menu as in Header--></div>
-            <div class="contact-info">
-                <h4>Contact Info</h4>
-                <ul>
-                    <li>Chicago 12, Melborne City, USA</li>
-                    <li><a href="tel:+8801682648101">+88 01682648101</a></li>
-                    <li><a href="mailto:info@example.com">info@example.com</a></li>
-                </ul>
-            </div>
-            <div class="social-links">
-                <ul class="clearfix">
-                    <li><a href="#"><span class="fab fa-twitter"></span></a></li>
-                    <li><a href="#"><span class="fab fa-facebook-square"></span></a></li>
-                    <li><a href="#"><span class="fab fa-pinterest-p"></span></a></li>
-                    <li><a href="#"><span class="fab fa-instagram"></span></a></li>
-                    <li><a href="#"><span class="fab fa-youtube"></span></a></li>
-                </ul>
-            </div>
-        </nav>
-    </div><!-- End Mobile Menu -->
-
-
-    <!-- page-title -->
-    <section class="page-title" style="background-image: url(images/background/pagetitle-bg.png);">
-        <div class="anim-icons">
-            <div class="icon icon-1"><img src="images/icons/anim-icon-17.png" alt=""></div>
-            <div class="icon icon-2 rotate-me"><img src="images/icons/anim-icon-18.png" alt=""></div>
-            <div class="icon icon-3 rotate-me"><img src="images/icons/anim-icon-19.png" alt=""></div>
-            <div class="icon icon-4"></div>
-        </div>
-        <div class="container">
-            <div class="content-box clearfix">
+  @section('name_page')
+  <div class="content-box clearfix">
                 <div class="title-box pull-left">
                     <h1>NOSOTROS</h1>
                     <p>Pacificode te ofrece tener un sitio web o App unico a la medida.</p>
@@ -55,9 +12,13 @@
                     <li><a href="index.html">Home</a></li>
                 </ul>
             </div>
-        </div>
-    </section>
-    <!-- page-title end -->
+
+  @endsection
+
+  @section('content') 
+
+
+   
 
 
     <!-- our-history -->
@@ -72,16 +33,18 @@
                                 <p>Somos un grupo de profesionales en el área de ingeniería de sistemas, apasionados por esta profesión nace pacificode. Empresa de desarrollo web a la medida que tiene como principal objetivo suplir las necesidades y requerimientos de su empresa o negocio.</p>
                                 <p>Pacificode tiene como lema ¡Desarrollo de sitios a la medida!, garantizando competitividad en el mercado.</p>
                                 <p>En los últimos años hemos adquirido las competencias para brindar los mejores servicios, este grupo de profesionales comprometidos con especializarse con las últimas tecnologías desea suplir tus necesidades con un alto nivel de calidad.</p>
-                            </div>
-                            <h5>PACIFICODE</h5>
-                            <figure class="signatur"><img src="images/icons/signatur.png" alt=""></figure>
-                        </div>
+                           
                     </div>
+                    </div>
+                            
+                    </div> 
+
+
                 </div>
                 <div class="col-lg-6 col-md-12 col-sm-12 image-column">
                     <div id="image_block_47">
                         <div class="image-box js-tilt">
-                            <figure class="image wow slideInRight" data-wow-delay="00ms" data-wow-duration="1500ms"><img src="images/resource/illustration-41.png" alt=""></figure>
+                            <figure class="image wow slideInRight" data-wow-delay="00ms" data-wow-duration="1500ms"><img src="images/gallery/team_development.png" alt=""></figure>
                         </div>
                     </div>
                 </div>
@@ -89,90 +52,91 @@
         </div>
     </section>
     <!-- our-history end -->
+-----------------------------------------------------
 
-
-    <!-- video-section -->
-    <section class="video-section">
-        <div class="bg-column" style="background-image: url(images/background/video-bg.jpg);"></div>
+    <!-- our-team -->
+    <section class="team-style-three centred">
         <div class="container">
             <div class="row">
-                <div class="col-lg-6 col-md-12 col-sm-12 video-column">
-                    <div class="video-inner">
-                        <a href="https://www.youtube.com/watch?v=nfP5N9Yc72A&amp;t=28s" class="lightbox-image" data-caption="">
-                            <i class="flaticon-play-button"></i>
-                            <span class="ripple"></span>
-                        </a>
-                    </div>
-                </div>
-                <div class="col-lg-6 col-md-12 col-sm-12 content-column">
-                    <div id="content_block_04">
-                        <div class="content-box">
-                            <div class="sec-title"><h2>Video Demo App</h2></div>
-                            <div class="text">Retarget past customers with second-chance offers and reach new audiences with geo-targeted campaigns during peak dining times using Forks’ push notifications.</div>
-                            <div class="btn-box"><a href="#" class="theme-btn-two">Play Video Now</a></div>
+                <div class="col-lg-3 col-md-6 col-sm-12 team-block">
+                    <div class="team-block-three">
+                        <div class="inner-box">
+                            <figure class="image-box">
+                                <img src="images/resource/team-4.png" alt="">
+                                <ul class="social-links clearfix">
+                                    <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-skype"></i></a></li>
+                                </ul>
+                            </figure>
+                            <div class="lower-content">
+                                <h5><a href="#">Edward Valencia Ortiz</a></h5>
+                                <span class="designation">Graphic Designer</span>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>
+                <div class="col-lg-3 col-md-6 col-sm-12 team-block">
+                    <div class="team-block-three">
+                        <div class="inner-box">
+                            <figure class="image-box">
+                                <img src="images/resource/team-5.png" alt="">
+                                <ul class="social-links clearfix">
+                                    <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-skype"></i></a></li>
+                                </ul>
+                            </figure>
+                            <div class="lower-content">
+                                <h5><a href="#">Kevin J. Richard Rosero</a></h5>
+                                <span class="designation">UI/UX Designner</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-12 team-block">
+                    <div class="team-block-three">
+                        <div class="inner-box">
+                            <figure class="image-box">
+                                <img src="images/resource/team-6.png" alt="">
+                                <ul class="social-links clearfix">
+                                    <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-skype"></i></a></li>
+                                </ul>
+                            </figure>
+                            <div class="lower-content">
+                                <h5><a href="#">Henry Stivet Montaño</a></h5>
+                                <span class="designation">Digital Marketer</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 col-sm-12 team-block">
+                    <div class="team-block-three">
+                        <div class="inner-box">
+                            <figure class="image-box">
+                                <img src="images/resource/team-7.png" alt="">
+                                <ul class="social-links clearfix">
+                                    <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+                                    <li><a href="#"><i class="fab fa-skype"></i></a></li>
+                                </ul>
+                            </figure>
+                            <div class="lower-content">
+                                <h5><a href="#">Andy Stuar Caicedo</a></h5>
+                                <span class="designation">UI/UX Designner</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+               
     </section>
-    <!-- video-section end -->
-
-
-    <!-- counter-style-three -->
-    <section class="counter-style-three">
-        <div class="anim-icons">
-            <div class="icon icon-1 rotate-me"></div>
-            <div class="icon icon-2"></div>
-            <div class="icon icon-3 rotate-me"></div>
-        </div>
-        <div class="container">
-            <div class="sec-title center">
-                <h2>Some Funfucts Shear</h2>
-                <p>The full monty burke posh excuse my French Richard cheeky bobby spiffing crikey<br />Why gormless, pear shaped.!</p>
-            </div>
-            <div class="row">
-                <div class="col-lg-3 col-md-6 col-sm-12 counter-block">
-                    <div class="inner-box wow zoomIn animated" data-wow-delay="00ms" data-wow-duration="1500ms">
-                        <div class="layer-bg" style="background-image: url(images/icons/pattern-25.png);"></div>
-                        <div class="count-outer count-box">
-                            <span class="count-text" data-speed="1500" data-stop="693">0</span>
-                        </div>
-                        <div class="text">Clientes felices</div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6 col-sm-12 counter-block">
-                    <div class="inner-box wow zoomIn animated" data-wow-delay="300ms" data-wow-duration="1500ms">
-                        <div class="layer-bg" style="background-image: url(images/icons/pattern-26.png);"></div>
-                        <div class="count-outer count-box">
-                            <span class="count-text" data-speed="1500" data-stop="453">0</span>
-                        </div>
-                        <div class="text">Usuarios de confianza</div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6 col-sm-12 counter-block">
-                    <div class="inner-box wow zoomIn animated" data-wow-delay="600ms" data-wow-duration="1500ms">
-                        <div class="layer-bg" style="background-image: url(images/icons/pattern-27.png);"></div>
-                        <div class="count-outer count-box">
-                            <span class="count-text" data-speed="1500" data-stop="276">0</span>
-                        </div>
-                        <div class="text">Nuestros Proyectos</div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6 col-sm-12 counter-block">
-                    <div class="inner-box wow zoomIn animated" data-wow-delay="900ms" data-wow-duration="1500ms">
-                        <div class="layer-bg" style="background-image: url(images/icons/pattern-28.png);"></div>
-                        <div class="count-outer count-box">
-                            <span class="count-text" data-speed="1500" data-stop="93">0</span>
-                        </div>
-                        <div class="text">Nuestros premios</div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <!-- counter-style-three end -->
-
+    <!-- team-style-three end -->
 
     <!-- subscribe-style-five -->
     <section class="subscribe-style-five">
@@ -189,12 +153,12 @@
                 <div class="col-lg-6 col-md-12 col-sm-12 content-column">
                     <div id="content_block_41">
                         <div class="content-box">
-                            <div class="sec-title"><h2>Subscribe our Newsletter</h2></div>
-                            <div class="text">Lorem ipsum dolor sit amet consectetur adipiscing elit donec tempus pellentesque dui vel tristique purus justo</div>
+                            <div class="sec-title"><h2>Suscríbase a nuestro boletín</h2></div>
+                            <div class="text">Se el primero en suscribirte. Te sorprenderás con nuestros servicios con la más alta calidad, anímate ya. </div>
                             <form action="#" method="post" class="subscribe-form">
                                 <div class="form-group">
-                                    <input type="email" name="email" placeholder="Enter Your Email" required="">
-                                    <button type="submit" class="theme-btn-two">Subscribe Now</button>
+                                    <input type="email" name="email" placeholder="Introduce tu correo electrónico" required="">
+                                    <button type="submit" class="theme-btn-two">Suscríbase ahora</button>
                                 </div>
                             </form>
                         </div>
