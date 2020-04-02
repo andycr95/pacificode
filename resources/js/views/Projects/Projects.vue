21f944d2722d5344f03463395b77e08bba16daf7
henry stivent <henrystiven7@hotmail.com>
2020-04-01 21:25:54 -0500

    Crud de projects Completo en el admin

--- resources/js/views/Projects/Projects.vue
+++ resources/js/views/Projects/Projects.vue
@@ -28,17 +28,18 @@
                             </div>
                         </div>
                     </div>
-                    <div class="page-body" v-for="p in projects" v-bind:key="p.id">
+                    <div class="page-body" v-for="p in projects" v-bind:key="p.id" style="padding-bottom:10px;">
                         <div class="row">
                             <div class="col-md-12">
                                 <div>
                                     <div class="bg-white p-relative">
-                                        <div class="input-group wall-elips">
-                                            <span class="dropdown-toggle addon-btn text-muted f-right wall-dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true" role="tooltip"></span>
-                                            <div class="dropdown-menu dropdown-menu-right b-none services-list">
-                                                <a class="dropdown-item" href="#">Report Photo</a>
-                                                <a class="dropdown-item" href="#">Hide From Timeline</a>
-                                                <a class="dropdown-item" href="#">Blog User</a>
+                                        <div class="card-block">
+                                            <div class="wall-elips">
+                                              <span class="dropdown-toggle addon-btn text-muted f-right wall-dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true" role="tooltip"></span>
+                                              <div class="dropdown-menu dropdown-menu-right b-none services-list">
+                                                <router-link class="dropdown-item":to="{name: 'updateproject',params: {id: p.id}}">Actualizar Proyecto</router-link>
+                                                <a class="dropdown-item" @click="deleteProject(p.id)">Eliminar Proyecto</a>
+                                              </div>
                                             </div>
                                         </div>
                                         <div class="card-block">
@@ -55,11 +56,9 @@
                                             </div>
                                         </div>
                                         <div id="lightgallery" class="lightgallery-popup">
-                                            <div class="" data-responsive="/files/assets/images/timeline/img1.jpg 375, img/1-480.jpg 480, img/1.jpg 800" data-src="/files/assets/images/timeline/img1.jpg" data-sub-html="<h4>Fading Light</h4><p>Classic view from Rigwood Jetty on Coniston Water an old archive shot similar to an old post but a little later on.</p>">
-                                                <a href="">
-                                                    <img src="/files/assets/images/timeline/img1.jpg" class="img-fluid width-100" alt="">
-                                                </a>
-                                            </div>
+                                          <a href="">
+                                              <img :src="p.project_photo" class="img-fluid width-100" alt="">
+                                          </a>
                                         </div>
                                         <div class="card-block">
                                             <div class="timeline-details">
@@ -94,27 +93,56 @@
 
 <script>
 import Axios from "axios";
+import Toastr from "toastr";
     export default {
         mounted() {
           this.getProjects()
         },
         data(){
             return {
-                projects:[]
+                projects:[],
+                pagination: {}
             }
         },
         methods:{
-          showModal(){
-
-          },async getProjects() {
-             await Axios.get('/api/projects',{headers:{'Authorization':this.$session.get('Authorization'), 'Accept':'application/json'}}).then(res =>{
+             async getProjects() {
+                await Axios.get('/api/projects').then(res =>{
 
                  this.projects = res.data;
                   console.log(this.projects);
              }).catch(err =>{
                  console.log(err);
              })
-          }
-       }
-    }
+          },async deleteProject(id) {
+              let options = {
+                  html: false,
+                  loader: false,
+                  reverse: false,
+                  cancelText: "Cancelar",
+                  animation: "zoom",
+                  type: "fade",
+                  clicksCount: 3,
+                  backdropClose: true,
+                  customClass: ""
+              };
+              let auth = this;
+              this.$dialog
+                  .confirm("Está seguro de eliminar este proyecto?", options)
+                  .then(async function(dialog) {
+                      await Axios.delete(`/api/project/` + id, {
+                          headers: {
+                              Accept: "application/json",
+                              Authorization: auth.$session.get("Authorization")
+                          }
+                      }).then(res => {
+                              auth.getProjects();
+                              Toastr.info(res.data);
+                          })
+                          .catch(err => {
+                              console.log(err);
+                          });
+                  });
+                }
+              }
+            }
 </script>
